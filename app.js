b4f7f4cac7072e0ed7050ec42c36b57972ddec84
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-05-06 14:37:34 +0200

    creando nivel 4

---
+++ app.js
@@ -0,0 +1,57 @@
+//https://codepen.io/alvaro624la/pen/qBXXVmx
+const coleccionNiveles = [
+    {nivel: 1, titulo: 'Calentamiento nº1: Array', descripcion: 'Vamos a empezar calentando con los conceptos más básicos. Empecemos por añadir valores a éste array', problema: "let mascotas = ['perro'];", planteamiento: 'Añade, al principio del array, el nuevo "gato" que voy a adoptar para hacer compañia a mi perro'},
+    {nivel: 2, titulo: 'Calentamiento nº2: Array', descripcion: 'Continuamos calentando. Ahora me ha entrado un ratón en casa... ¡está entre mis mascotas!', problema: "let mascotas = ['gato', 'raton', 'perro'];", planteamiento: 'Modifica el array eliminando el "raton" de entre mis queridas mascotas'},
+    {nivel: 3, titulo: 'Calentamiento nº3: Array', descripcion: 'Me encantan los animales, por ello, he decidido presentarme voluntaria y pasear a todos éstos perros. Ayudame a contarlos rápidamente para poder apuntarlo en el registro de la perrera', problema: "let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Gin', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen']; let cantidad = null;", planteamiento: '¿Cuál és el número exacto de perros que tengo que pasear hoy? Asigna el valor resultante a la variable cantidad'},
+    {nivel: 4, titulo: 'Calentamiento nº4: Array', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 5, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 6, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 7, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 8, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 9, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 10, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 12, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 12, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 13, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 14, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 15, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 16, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 17, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 18, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 19, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 20, titulo: '', descripcion: '', problema: "", planteamiento: ''},
+    {nivel: 21, titulo: '', descripcion: 'Hoy tengo comida familiar. Quiero poner el nombre de los invitados en las sillas, pero tengo poco espacio para escribir a mi sobrino', problema: "let sobrino = 'Alejandro Daniel Ramirez'", planteamiento: '¿Cuántas palabras/carácteres contiene exactamente el nombre de "Alejandro"? Asigna el valor resultante a la variable "palabras"'},
+];
+
+//COMPROBACIONES (cambiar de documento al terminar comprobaciones (visible para el cliente))
+//NIVEL 1
+// var mascotas = ['perro']; 
+// mascotas.unshift('gato'); 
+// console.log(mascotas);
+
+//NIVEL 2
+// var mascotas = ['gato', 'raton', 'perro'];
+// mascotas.splice(1, 1)
+// console.log(mascotas);
+
+//NIVEL 3
+// let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Gin', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen'];
+// let cantidad = null;
+// cantidad = perros.length;
+// console.log(cantidad);
+
+
+
+
+
+
+//NIVEL x
+let sobrino = 'Alejandro Daniel Ramirez';
+let palabras = null;
+
+//contar palabras caracteres string javascript:
+// Quitar los espacios en blanco y pasar el string a minúsculas(si es necesario)
+// Recorrer la cadena de texto en vez de pasarlo a un array.
+// Finalmente guardar el resultado en un object, donde la letra es la key y su total.
+
+console.log(palabras);
---
+++ bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app.js":
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
+/***/ (() => {
+
+eval("//https://codepen.io/alvaro624la/pen/qBXXVmx\nvar coleccionNiveles = [{\n  nivel: 1,\n  titulo: 'Calentamiento nº1: Array',\n  descripcion: 'Vamos a empezar calentando con los conceptos más básicos. Empecemos por añadir valores a éste array',\n  problema: \"let mascotas = ['perro'];\",\n  planteamiento: 'Añade, al principio del array, el nuevo \"gato\" que voy a adoptar para hacer compañia a mi perro'\n}, {\n  nivel: 2,\n  titulo: 'Calentamiento nº2: Array',\n  descripcion: 'Continuamos calentando. Ahora me ha entrado un ratón en casa... ¡está entre mis mascotas!',\n  problema: \"let mascotas = ['gato', 'raton', 'perro'];\",\n  planteamiento: 'Modifica el array eliminando el \"raton\" de entre mis queridas mascotas'\n}, {\n  nivel: 3,\n  titulo: 'Calentamiento nº3: Array',\n  descripcion: 'Me encantan los animales, por ello, he decidido presentarme voluntaria y pasear a todos éstos perros. Ayudame a contarlos rápidamente para poder apuntarlo en el registro de la perrera',\n  problema: \"let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Gin', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen']; let cantidad = null;\",\n  planteamiento: '¿Cuál és el número exacto de perros que tengo que pasear hoy? Asigna el valor resultante a la variable cantidad'\n}, {\n  nivel: 4,\n  titulo: 'Calentamiento nº4: Array',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 5,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 6,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 7,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 8,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 9,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 10,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 12,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 12,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 13,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 14,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 15,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 16,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 17,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 18,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 19,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 20,\n  titulo: '',\n  descripcion: '',\n  problema: \"\",\n  planteamiento: ''\n}, {\n  nivel: 21,\n  titulo: '',\n  descripcion: 'Hoy tengo comida familiar. Quiero poner el nombre de los invitados en las sillas, pero tengo poco espacio para escribir a mi sobrino',\n  problema: \"let sobrino = 'Alejandro Daniel Ramirez'\",\n  planteamiento: '¿Cuántas palabras/carácteres contiene exactamente el nombre de \"Alejandro\"? Asigna el valor resultante a la variable \"palabras\"'\n}]; //COMPROBACIONES (cambiar de documento al terminar comprobaciones (visible para el cliente))\n//NIVEL 1\n// var mascotas = ['perro']; \n// mascotas.unshift('gato'); \n// console.log(mascotas);\n//NIVEL 2\n// var mascotas = ['gato', 'raton', 'perro'];\n// mascotas.splice(1, 1)\n// console.log(mascotas);\n//NIVEL 3\n// let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Gin', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen'];\n// let cantidad = null;\n// cantidad = perros.length;\n// console.log(cantidad);\n//NIVEL x\n\nvar sobrino = 'Alejandro Daniel Ramirez';\nvar palabras = null; //contar palabras caracteres string javascript:\n// Quitar los espacios en blanco y pasar el string a minúsculas(si es necesario)\n// Recorrer la cadena de texto en vez de pasarlo a un array.\n// Finalmente guardar el resultado en un object, donde la letra es la key y su total.\n\nconsole.log(palabras);\n\n//# sourceURL=webpack:///./app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app.js"]();
+/******/ 	
+/******/ })()
+;
