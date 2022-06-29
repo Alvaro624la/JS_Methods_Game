ef858ef4f004e915e115c9c17388200827554394
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-06-29 11:27:42 +0200

    feat: "Enter" key para comprobar + nuevos niveles
    Añadida funcionalidad de presionar la tecla enter dentro del textarea
    para comprobar el resultado, y nuevos niveles añadidos

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const coleccionNiveles = [
     {
         nivel: 3, 
         titulo: 'Calentamiento nº3: Array 🐶', 
-        descripcion: 'Me encantan los animales, por ello, he decidido presentarme voluntaria y pasear a todos éstos perros. Ayudame a contarlos rápidamente para poder apuntarlo en el registro de la perrera.', 
+        descripcion: 'He decidido presentarme voluntaria y pasear a todos éstos perros. Ayudame a contarlos rápidamente para poder apuntarlo en el registro de la perrera.', 
         problema: "let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen']; <br>let cantidad = <em>null</em>;", 
         planteamiento: '¿Cuál és el número exacto de perros que tengo que pasear hoy? Asigna el valor resultante a la variable <code>cantidad</code>.', 
         respuesta: {
@@ -51,9 +51,149 @@ const coleccionNiveles = [
             h: "cantidad =perros.length"
         },
         solucion: "let cantidad = 10;"
-    },    
+    },
     {
         nivel: 4, 
+        titulo: 'Nivel 1: Básico ☕',
+        descripcion: 'Creo que me faltaba aceite en casa...', 
+        problema: "let cintaCaj = ['patatas buenas', 'huevos'];",
+        planteamiento: '¿Cómo añado "aceite" al final de la cinta del cajero antes de que acabe de escánear toda la compra?',
+        respuesta: {
+            a: "cintaCaj.push('aceite');",
+            b: 'cintaCaj.push("aceite");',
+            c: "cintaCaj.push('aceite')",
+            d: 'cintaCaj.push("aceite")',
+            e: "cintaCaj.push(' aceite ');",
+            f: 'cintaCaj.push(" aceite ");',
+            g: "cintaCaj.push(' aceite ')",
+            h: 'cintaCaj.push(" aceite ")',
+            i: '',
+            j: ''
+        },
+        solucion: "cintaCaj = ['patatas buenas', 'huevos', 'aceite'];"
+    },
+    {
+        nivel: 5, 
+        titulo: 'Nivel 2: Básico 🥚',
+        descripcion: 'Ya tengo todo preparado para empezar la tortilla, pero me he dado cuenta de que hay patatas en mal estado', 
+        problema: "let productos = ['patatas pochas', 'patatas buenas', 'huevos', 'aceite'];",
+        planteamiento: '¿Cómo elimino las patatas pochas principio del array?',
+        respuesta: {
+            a: "productos.shift();",
+            b: 'productos.shift()',
+            c: "",
+            d: '',
+            e: "",
+            f: "",
+            g: '',
+            h: "",
+            i: '',
+            j: ""
+        },
+        solucion: "productos = ['patatas buenas', 'huevos', 'aceite'];"
+    },
+    {
+        nivel: 6, 
+        titulo: 'Nivel 3: Básico 🍅🧀',
+        descripcion: 'Voy a acompañar la tortilla con macarrones. En el armario tengo tres ingredientes, pero solo quiero tomate y queso.', 
+        problema: "let ingredientes = ['pasas', 'tomate', 'queso'];",
+        planteamiento: '¿Cómo parto o descarto éste primer ingrediente dejando los otros dos restantes?',
+        respuesta: {
+            a: "ingredientes.slice(1);",
+            b: 'ingredientes.slice(1)',
+            c: "ingredientes.slice( 1 );",
+            d: 'ingredientes.slice( 1 )',
+            e: "ingredientes.slice(1 );",
+            f: "ingredientes.slice(1 )",
+            g: 'ingredientes.slice( 1);',
+            h: "ingredientes.slice( 1)",
+            i: '',
+            j: ""
+        },
+        solucion: "ingredientes = ['tomate', 'queso'];"
+    },
+    {
+        nivel: 7, 
+        titulo: 'Nivel 4: Básico 💁',
+        descripcion: 'Hoy he ido a mi local de confianza y he visto que hay varias personas esperando.', 
+        problema: "let cola = ['Ana', 'Juan', 'Carlos', 'Marimar', 'Silvia'];",
+        planteamiento: '¿Cuál es el último índice en la posición de la cola del establecimiento? (La última sé que se llama Silvia)',
+        respuesta: {
+            a: "cola.lastIndexOf('Silvia');",
+            b: 'cola.lastIndexOf("Silvia");',
+            c: "cola.lastIndexOf('Silvia')",
+            d: 'cola.lastIndexOf("Silvia")',
+            e: "cola.lastIndexOf(' Silvia ');",
+            f: 'cola.lastIndexOf(" Silvia ");',
+            g: "cola.lastIndexOf(' Silvia ')",
+            h: 'cola.lastIndexOf(" Silvia ")',
+            i: '',
+            j: ""
+        },
+        solucion: "cola = 4; (interpretando que la posicion 0 es la primera)"
+    },
+    {
+        nivel: 8, 
+        titulo: 'Nivel 5: Básico 👀',
+        descripcion: 'Me parece haber visto a una amiga mía delante. Creo que es Ana.', 
+        problema: "let posicion = ['Ana', 'Juan', 'Carlos', 'Marimar', 'Silvia', 'yo'];",
+        planteamiento: '¿Cuál es el índice de Ana en el array?',
+        respuesta: {
+            a: "posicion.indexOf('Ana');",
+            b: 'posicion.indexOf("Ana");',
+            c: "posicion.indexOf('Ana')",
+            d: 'posicion.indexOf("Ana")',
+            e: "posicion.indexOf(' Ana ');",
+            f: 'posicion.indexOf(" Ana ");',
+            g: "posicion.indexOf(' Ana ')",
+            h: 'posicion.indexOf(" Ana ")',
+            i: '',
+            j: ""
+        },
+        solucion: "posicion = 0; (interpretando que la posición 0 es la primera)"
+    },
+    {
+        nivel: 9, 
+        titulo: 'Nivel 6: Básico 🔀',
+        descripcion: 'La cola la estaban haciendo al reves y ahora las posiciones de las personas se revertirán.', 
+        problema: "let colaReves = ['Ana', 'Juan', 'Carlos', 'Marimar', 'Silvia', 'yo'];",
+        planteamiento: '¿Como lo haríamos?',
+        respuesta: {
+            a: "colaReves.reverse();",
+            b: 'colaReves.reverse()',
+            c: "",
+            d: '',
+            e: "",
+            f: "",
+            g: '',
+            h: "",
+            i: '',
+            j: ""
+        },
+        solucion: "colaReves = ['yo', 'Silvia', 'Marimar', 'Carlos', 'Juan', 'Ana'];"
+    },
+    {
+        nivel: 10, 
+        titulo: 'Nivel 7: Básico 🎨',
+        descripcion: 'Llego a casa y continuo con el CSS. He escogido los colores para el backoground de mi página web pero el verde pistacho no me convence.', 
+        problema: "let paleta = ['rojo', 'negro', 'verde pistacho'];",
+        planteamiento: '¿Cómo puedo quitar éste último color del array?',
+        respuesta: {
+            a: "paleta.pop();",
+            b: 'paleta.pop()',
+            c: "",
+            d: '',
+            e: "",
+            f: "",
+            g: '',
+            h: "",
+            i: '',
+            j: ""
+        },
+        solucion: "paleta = ['rojo', 'negro'];"
+    },
+    {
+        nivel: 11, 
         titulo: 'Nivel 1: Junior Front-End 🤞', 
         descripcion: 'Invertir cadena', 
         problema: "let cadena = 'pato donald'; <br>let invertida = <em>undefined</em>;", 
@@ -71,7 +211,7 @@ const coleccionNiveles = [
         solucion: "let invertida = 'dlanod otap'"
     },
     {
-        nivel: 5, 
+        nivel: 12, 
         titulo: 'Nivel 2: Junior Front-End 🔢', 
         descripcion: 'He recibido ésta colección de datos:', 
         problema: "let datos = [4, 10, 5, 696, 32, 369, 1000, 12, 5, 1, 2, 4, 21, 22]; <br>let ordenar = <em>undefined</em>;", 
@@ -91,7 +231,7 @@ const coleccionNiveles = [
         solucion: "let ordenar = [1, 2, 4, 4, 5, 5, 10, 12, 21, 22, 32, 369, 696, 1000];"
     },
     {
-        nivel: 6, 
+        nivel: 13, 
         titulo: 'Nivel 3: Junior Front-End 🕵️‍♀️', 
         descripcion: 'Quiero saber el número total, de todas las "s" que hay en éste trabalenguas:', 
         problema: "let trabalenguas = 'Si tu gusto gustara del gusto que gusta mi gusto, mi gusto gustaría del gusto que gusta tu gusto'; <br>let repet = <em>null</em>;", 
@@ -106,11 +246,11 @@ const coleccionNiveles = [
             g: 'repet= trabalenguas.match(/s/gi).length',
             h: 'repet =trabalenguas.match(/s/gi).length'
         },
-        solucion: "let repet = '11'"
+        solucion: "let repet = '11';"
     },    
     {
-        nivel: 7, 
-        titulo: 'Nivel 5: Junior Front-End 🧮', 
+        nivel: 14, 
+        titulo: 'Nivel 4: Junior Front-End 🧮', 
         descripcion: '', 
         problema: "let cadena = 'El número pi 3,14 es muy largo. 3,141592653589793 y más. En binario es 11,001001 y más.'; <br>let cantidad = <em>null</em>;", 
         planteamiento: '¿Cuál es la cantidad de números que contiene esta cadena?. Asigna el valor a la variable <code>cantidad</code>.', 
@@ -127,7 +267,7 @@ const coleccionNiveles = [
         solucion: "let cantidad = 27;"
     },    
     {
-        nivel: 8, 
+        nivel: 15, 
         titulo: 'Finished!🎉', 
         descripcion: "Congratulations, you've finished the JS Methods Game.", 
         problema: "😁let JsMethodsGame = true;😁", 
@@ -166,7 +306,7 @@ const coleccionNiveles = [
         solucion: ""
     },    
     {
-        nivel: 10, 
+        nivel: 222, 
         titulo: 'Nivel 7: Junior Front-End', 
         descripcion: 'Distancia de Hamming', 
         problema: "", 
@@ -207,62 +347,22 @@ const coleccionNiveles = [
     }
 ];
 
-//COMPROBACIONES (cambiar de documento al terminar comprobaciones (visible para el cliente))
-//NIVEL 1
-// var mascotas = ['perro']; 
-// mascotas.unshift('gato');
-// console.log(mascotas);
-// //comprobar
-// console.log(mascotas[0].includes('gato'));
+//IDEAS NUEVOS NIVELES
 
-//NIVEL 2
-// var mascotas = ['gato', 'raton', 'perro'];
-// mascotas.splice(1, 1);
-// console.log(mascotas);
-// let car;
-// console.log(car);
+//-------NOSE-------
+// a = ['coche', 'moto'];
+// console.log(a.concat('bici'));
+//resultado: a = ;
 
-//NIVEL 3
-// let perros = ['Luby', 'Luna', 'Toby', 'Leo', 'Gin', 'Coco', 'Harry', 'Nelson', 'July', 'Gabe', 'Darleen'];
-// let cantidad = null;
-// cantidad = perros.length;
-// console.log(cantidad);
+//-------NOSE-------
+//let b = ['Álvaro', 'Prado'];
+// console.log(b.join(' de '));
+//resultado: b = ;
 
-//NIVEL 4
-// let cadena = 'pato donald';
-// let invertida = undefined;
-// invertida = cadena.split('').reverse().join('');
-// console.log(invertida);
-
-//NIVEL 5
-// let datos = [4, 10, 5, 696, 32, 369, 1000, 12, 5, 1, 2, 4, 21, 22];
-// let ordenar = undefined;
-// ordenar = datos.sort((a, b)=> a - b);
-// console.log(ordenar);
-
-//NIVEL 6
-// let trabalenguas = 'Si tu gusto gustara del gusto que gusta mi gusto, mi gusto gustaría del gusto que gusta tu gusto';
-// let repet = null;
-// repet = trabalenguas.match(/s/gi).length;
-// console.log(repet);
-
-//NIVEL 7
-// let cadena = 'El número pi 3,14 es muy largo. 3,141592653589793 y más. En binario es 11,001001 y más.';
-// let cantidad;
-// cantidad = cadena.match(/[0-9]/g).length;
-// console.log(cantidad);
-
-//NIVEL 8
-
-
-
-
-
-
-
-
-
-
+//-------NOSE-------
+// let g = ['1', '2', '3'];
+// console.log(g.sort());
+//resultado: 
 
 //NIVEL x
 // let sobrino = 'Alejandro Daniel Ramirez';
@@ -289,7 +389,6 @@ const body = document.getElementById('body');
 let modal;
 let content;
 let modalBtn;
-
 
 //CREACIÓN NODOS
 //NIVELES
@@ -361,7 +460,7 @@ let nuevoNivel = () => {
                 modalBtn.classList.add('body__modal__close-btn');
 
             //GAME FINISHED/PASSED
-            if(acc === 8){
+            if(acc === 15){
                 solucionContenido1.innerHTML = ``;
                 solucionContenido2.innerHTML = ``;
                 //clear local storage btn
@@ -375,71 +474,82 @@ let nuevoNivel = () => {
         };
     });
 
-
     //COMPROBACIÓN RESULTADO
     //VARIABLES (global)
     try{
         let respuesta = document.getElementById('solText');
-        let comprobarBtn = document.getElementById('solBtn'); 
-   
+        let comprobarBtn = document.getElementById('solBtn');
+
     //FUNCIONES
-    comprobarBtn.addEventListener('click', ()=>{
-            if(
-                respuesta.value === nivelRespuesta.a || 
-                respuesta.value === nivelRespuesta.b || 
-                respuesta.value === nivelRespuesta.c || 
-                respuesta.value === nivelRespuesta.d || 
-                respuesta.value === nivelRespuesta.e || 
-                respuesta.value === nivelRespuesta.f || 
-                respuesta.value === nivelRespuesta.g || 
-                respuesta.value === nivelRespuesta.h || 
-                respuesta.value === nivelRespuesta.i || 
-                respuesta.value === nivelRespuesta.j
-                ){
-                //MODAL and NEXT LEVEL
-                modal.classList.add('body__modal--active');
-                content.innerHTML = `¡Correcto! El resultado es ➔ ${nivelSolucion}`;
-                modalBtn.classList.add('body__modal__close-btn--active');
-                main.style.filter = 'blur(2px)';
-                modalBtn.addEventListener('click', ()=>{
-                    modal.classList.remove('body__modal--active');
-                    modalBtn.classList.remove('body__modal__close-btn--active');
-                    main.style.filter = '';
-                    //NUEVO NIVEL
-                    nuevoNivel();
-                });
-                //TEXTAREA
-                respuesta.style.backgroundColor = 'rgba(0, 255, 0, .6)';
-                setTimeout(returnColor, 800);
-                function returnColor(){
-                    respuesta.style.backgroundColor = '#fff';
-                };
-                //CONTADOR
-                acc++;
-                //local storage CONTADOR
-                localStorage.setItem('contador', JSON.stringify(acc));
-            } else {
-                //MODAL AND NO PASS LEVEL
-                modal.classList.add('body__modal--active');
-                modal.style.boxShadow = 'rgba(255, 0, 0, 0.3) 0px 0px 0px 4px';
-                content.innerHTML = `Incorrecto (recuerda utilizar ES6 y/o escribir limpio y legible con espacios correspondientes)`;
-                modalBtn.classList.add('body__modal__close-btn--active');
-                main.style.filter = 'blur(2px)';
-                modalBtn.addEventListener('click', ()=>{
-                    modal.classList.remove('body__modal--active');
-                    modal.style.boxShadow = 'rgba(3, 52, 214, 0.3) 0px 0px 0px 4px';
-                    modalBtn.classList.remove('body__modal__close-btn--active');
-                    main.style.filter = '';
-                });
-                //TEXTAREA
-                respuesta.style.backgroundColor = 'rgba(255, 0, 0, .6)';
-                setTimeout(returnColor, 800);
-                function returnColor(){
-                    respuesta.style.backgroundColor = '#fff';
-                };
-            }
-        // }
+    //Comprobar:
+    let check = ()=>{
+        if(
+            respuesta.value === nivelRespuesta.a || 
+            respuesta.value === nivelRespuesta.b || 
+            respuesta.value === nivelRespuesta.c || 
+            respuesta.value === nivelRespuesta.d || 
+            respuesta.value === nivelRespuesta.e || 
+            respuesta.value === nivelRespuesta.f || 
+            respuesta.value === nivelRespuesta.g || 
+            respuesta.value === nivelRespuesta.h || 
+            respuesta.value === nivelRespuesta.i || 
+            respuesta.value === nivelRespuesta.j
+            ){
+            //MODAL and NEXT LEVEL
+            modal.classList.add('body__modal--active');
+            content.innerHTML = `¡Correcto! El resultado es ➔ ${nivelSolucion}`;
+            modalBtn.classList.add('body__modal__close-btn--active');
+            main.style.filter = 'blur(2px)';
+            modalBtn.addEventListener('click', ()=>{
+                modal.classList.remove('body__modal--active');
+                modalBtn.classList.remove('body__modal__close-btn--active');
+                main.style.filter = '';
+                //NUEVO NIVEL
+                nuevoNivel();
+            });
+            //TEXTAREA
+            respuesta.style.backgroundColor = 'rgba(0, 255, 0, .6)';
+            setTimeout(returnColor, 800);
+            function returnColor(){
+                respuesta.style.backgroundColor = '#fff';
+            };
+            //CONTADOR
+            acc++;
+            //local storage CONTADOR
+            localStorage.setItem('contador', JSON.stringify(acc));
+        } else {
+            //MODAL AND NO PASS LEVEL
+            modal.classList.add('body__modal--active');
+            modal.style.boxShadow = 'rgba(255, 0, 0, 0.3) 0px 0px 0px 4px';
+            content.innerHTML = `Incorrecto (recuerda utilizar ES6 y/o escribir limpio y legible con espacios correspondientes)`;
+            modalBtn.classList.add('body__modal__close-btn--active');
+            main.style.filter = 'blur(2px)';
+            modalBtn.addEventListener('click', ()=>{
+                modal.classList.remove('body__modal--active');
+                modal.style.boxShadow = 'rgba(3, 52, 214, 0.3) 0px 0px 0px 4px';
+                modalBtn.classList.remove('body__modal__close-btn--active');
+                main.style.filter = '';
+            });
+            //TEXTAREA
+            respuesta.style.backgroundColor = 'rgba(255, 0, 0, .6)';
+            setTimeout(returnColor, 800);
+            function returnColor(){
+                respuesta.style.backgroundColor = '#fff';
+            };
+        }
+    };
+    //Comprobar al presionar y soltar tecla ENTER
+    respuesta.addEventListener('keypress', function(e) {
+        if (e.code == 'Enter') {
+        e.preventDefault();
+        check();
+        };
     });
+    //Comprobar con click en btn "comprobar"
+    comprobarBtn.addEventListener('click', ()=>{
+        check();
+    });
+
     //clear local storage
     let clearBtn = document.getElementById('clearLocalStorageBtn');
 
@@ -462,3 +572,9 @@ window.onload = ()=>{
         nuevoNivel();
     };
 };
+
+//FALTA:
+//Añadir esc al boton del modal para cerrarlo
+//Opcion de mostrar niveles en orden aleatorio
+//Guardar en BBDD la cantidad de usuarios entrados en la pag.
+//Guardar en BBDD la cantidad/% de errores y aciertos, y en qué niveles.
